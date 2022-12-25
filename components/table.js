166526503e18fd636c3035ea144a5c87dfcb6ea2
Mehmet Fatih Yildiz <fatih@mfyz.com>
2022-12-24 21:32:16 -0500

    moved table to a component

---
+++ components/table.js
@@ -0,0 +1,87 @@
+import { useState } from 'react'
+import { CSVLink } from 'react-csv'
+
+import {
+	flexRender,
+	getCoreRowModel,
+	useReactTable,
+	// Column,
+	// Table,
+	// ColumnFiltersState,
+	// getFilteredRowModel,
+	// getFacetedRowModel,
+	// getFacetedUniqueValues,
+	// getFacetedMinMaxValues,
+	getSortedRowModel,
+	// FilterFn,
+	// FilterFns,
+} from '@tanstack/react-table'
+
+export default function ({ data, columns }) {
+	const table = useReactTable({
+		data,
+		columns,
+		getCoreRowModel: getCoreRowModel(),
+		getSortedRowModel: getSortedRowModel(),
+	})
+
+	return (
+		<div>
+			<div className="mb-2">
+				<CSVLink
+					data={data}
+					filename="export.csv"
+					className="bg-white hover:bg-gray-100 text-gray-800 py-1 px-2 border border-gray-400 rounded text-xs"
+				>
+					Export CSV
+				</CSVLink>
+			</div>
+			<div className='overflow-x-auto w-full border-2 border-gray-200'>
+				<table className='w-full table-auto whitespace-nowrap rounded-lg bg-white divide-y divide-gray-300 overflow-hidden'>
+					<thead className="bg-gray-50">
+						{table.getHeaderGroups().map(headerGroup => (
+							<tr key={headerGroup.id} className="text-gray-600 text-left">
+							{headerGroup.headers.map(header => (
+								<th key={header.id} className="font-semibold text-sm uppercase p-2">
+									{header.isPlaceholder
+										? null
+										: (
+											<div
+												{...{
+													className: header.column.getCanSort()
+														? 'cursor-pointer select-none'
+														: '',
+													onClick: header.column.getToggleSortingHandler(),
+												}}
+											>
+												{flexRender(
+													header.column.columnDef.header,
+													header.getContext()
+												)}
+												{{
+													asc: ' 🔼',
+													desc: ' 🔽',
+												}[header.column.getIsSorted()] ?? null}
+											</div>
+										)}
+								</th>
+							))}
+							</tr>
+						))}
+					</thead>
+					<tbody className="divide-y divide-gray-200">
+						{table.getRowModel().rows.map(row => (
+							<tr key={row.id}>
+								{row.getVisibleCells().map(cell => (
+									<td key={cell.id} className="p-2">
+										{flexRender(cell.column.columnDef.cell, cell.getContext())}
+									</td>
+								))}
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+		</div>
+	)
+}
--- pages/table.js
+++ pages/table.js
@@ -1,26 +1,9 @@
 import Head from 'next/head'
 import { useState } from 'react'
-import { CSVLink } from 'react-csv'
-
-import {
-	createColumnHelper,
-	flexRender,
-	getCoreRowModel,
-	useReactTable,
-
-	// Column,
-	// Table,
-	// ColumnFiltersState,
-	// getFilteredRowModel,
-	// getFacetedRowModel,
-	// getFacetedUniqueValues,
-	// getFacetedMinMaxValues,
-	getSortedRowModel,
-	// FilterFn,
-	// FilterFns,
-} from '@tanstack/react-table'
+import { createColumnHelper } from '@tanstack/react-table'
 
 import { H1 } from '../components/headings'
+import Table from '../components/table'
 
 const SectionTitle = ({ children }) => <H1 className="mt-4">{children}</H1>
 
@@ -77,14 +60,7 @@ const columns = [
 	}),
 ]
 
-export default function Table() {
-	const table = useReactTable({
-		data,
-		columns,
-		getCoreRowModel: getCoreRowModel(),
-		getSortedRowModel: getSortedRowModel(),
-	})
-
+export default function ExampleTable() {
 	return (
 		<div>
 			<Head>
@@ -92,61 +68,10 @@ export default function Table() {
 			</Head>
 			<main>
 				<SectionTitle>React Table</SectionTitle>
-				<div className="mb-2">
-					<CSVLink
-						data={data}
-						filename="export.csv"
-						className="bg-white hover:bg-gray-100 text-gray-800 py-1 px-2 border border-gray-400 rounded text-xs"
-					>
-						Export CSV
-					</CSVLink>
-				</div>
-				<div className='overflow-x-auto w-full border-2 border-gray-200'>
-					<table className='w-full table-auto whitespace-nowrap rounded-lg bg-white divide-y divide-gray-300 overflow-hidden'>
-						<thead className="bg-gray-50">
-							{table.getHeaderGroups().map(headerGroup => (
-								<tr key={headerGroup.id} className="text-gray-600 text-left">
-								{headerGroup.headers.map(header => (
-									<th key={header.id} className="font-semibold text-sm uppercase p-2">
-										{header.isPlaceholder
-											? null
-											: (
-												<div
-													{...{
-														className: header.column.getCanSort()
-															? 'cursor-pointer select-none'
-															: '',
-														onClick: header.column.getToggleSortingHandler(),
-													}}
-												>
-													{flexRender(
-														header.column.columnDef.header,
-														header.getContext()
-													)}
-													{{
-														asc: ' 🔼',
-														desc: ' 🔽',
-													}[header.column.getIsSorted()] ?? null}
-												</div>
-											)}
-									</th>
-								))}
-								</tr>
-							))}
-						</thead>
-						<tbody className="divide-y divide-gray-200">
-							{table.getRowModel().rows.map(row => (
-								<tr key={row.id}>
-									{row.getVisibleCells().map(cell => (
-										<td key={cell.id} className="p-2">
-											{flexRender(cell.column.columnDef.cell, cell.getContext())}
-										</td>
-									))}
-								</tr>
-							))}
-						</tbody>
-					</table>
-				</div>
+				<Table
+					data={data}
+					columns={columns}
+				/>
 			</main>
 		</div>
 	)
